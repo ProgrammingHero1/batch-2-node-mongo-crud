8932d8dd3b9ab8366259cc4505a56d6a98aed467
jhankarpHero <61257321+jhankarpHero@users.noreply.github.com>
2020-09-24 19:52:36 -0500

    after patch

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
 const password = 'yQgwjNcRYJiVdX5';
 
@@ -7,18 +9,61 @@ const uri = "mongodb+srv://organicUser:yQgwjNcRYJiVdX5@cluster0.bzdnt.mongodb.ne
 const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.send('hello I am working')
+    res.sendFile(__dirname + '/index.html');
 })
 
 
 
 
 client.connect(err => {
-  const collection = client.db("organicdb").collection("products");
-  console.log('databae connected')
-  client.close();
+  const productCollection = client.db("organicdb").collection("products");
+  
+  app.get('/products', (req, res) => {
+    productCollection.find({})
+    .toArray( (err, documents) =>{
+      res.send(documents);
+    })
+  })
+
+  app.get('/product/:id', (req, res) => {
+    productCollection.find({_id: ObjectId(req.params.id)})
+    .toArray ( (err, documents) =>{
+      res.send(documents[0]);
+    })
+  })
+  
+  app.post("/addProduct", (req, res) => {
+    const product = req.body;
+    productCollection.insertOne(product)
+    .then(result => {
+      console.log('data added successfully');
+      res.send('success');
+    })
+  })
+
+  app.patch('/update/:id', (req, res) => {
+    console.log(req.body.price);
+    productCollection.updateOne({_id: ObjectId(req.params.id)},
+    {
+      $set: {price: req.body.price, quantity: req.body.quantity}
+    })
+    .then (result => {
+      console.log(result);
+    })
+  })
+
+
+  app.delete('/delete/:id', (req, res) =>{
+    productCollection.deleteOne({_id: ObjectId(req.params.id)})
+    .then( result => {
+      console.log(result);
+    })
+  })
+
 });
 
 
